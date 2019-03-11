016b206a53edb13776dd4b37662ff4da08161df6
Simon Amarasingham <simon@dsonic.com>
2019-03-11 06:43:56 -0700

    additional functionality for window scrolling, but still in progress

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ class App extends Component {
     this.nextKey = 0;
     this.cellWidth = null;
     this.cellHeight = null;
+    this.windowPosition =[];
+    this.touchStartPosition = [];
 
     // some constants, for easy reading
     this.GRID_SIZE = 3;
@@ -16,6 +18,56 @@ class App extends Component {
     this.BOTTOM = 2;
     this.LEFT = 0;
     this.RIGHT = 2;
+    this.HORIZ_THRESHOLD = 0.25;
+    this.VERT_THRESHOLD = 0.25;
+
+    // a range of directions, in radians, for each direction a cell can be found in
+    this.DIRECTION_RANGES = {
+      'east': [15/8 * Math.PI, 1/8 * Math.PI],
+      'northeast': [1/8 * Math.PI, 3/8 * Math.PI],
+      'north': [3/8 * Math.PI, 5/8 * Math.PI],
+      'northwest': [5/8 * Math.PI, 7/8 * Math.PI],
+      'west': [7/8 * Math.PI, 9/8 * Math.PI],
+      'southwest': [9/8 * Math.PI, 11/8 * Math.PI],
+      'south': [11/8 * Math.PI, 13/8 * Math.PI],
+      'southeast': [13/8 * Math.PI, 15/8 * Math.PI],
+    }
+
+    this.DIRECTION_VECTORS = {
+      'east': [1,0],
+      'northeast': [1,1],
+      'north': [0,1],
+      'northwest': [-1, 1],
+      'west': [-1,0],
+      'southwest': [-1,-1],
+      'south': [0,-1],
+      'southeast': [1,-1],
+    }
+
+    // handle all touch events
+    //onTouchStart={(evt) => this.handleTouchStart(evt)}
+    //onTouchMove={(evt) => this.handleTouchMove(evt)}
+    //onTouchCancel={(evt) => this.handleTouchCancel(evt)}
+    //onTouchEnd={(evt) => this.handleTouchEnd(evt)}
+
+    window.addEventListener("touchstart", (evt) => {
+      //evt.preventDefault ();
+      this.handleTouchStart(evt);
+    }, {passive: false});
+
+    window.addEventListener("touchmove", (evt) => {
+      evt.preventDefault();
+      this.handleTouchMove(evt);
+    }, {passive: false});
+
+    //window.addEventListener("touchcanel", (evt) => {
+      //evt.preventDefault ();
+      //this.handleTouchCancel(evt);
+  //  }, {passive: false});
+    //window.addEventListener("touchend", (evt) => {
+      //s   evt.preventDefault ();
+      //this.handleTouchEnd(evt);
+    //}, {passive: false});
   }
 
   render() {
@@ -47,7 +99,10 @@ class App extends Component {
     let navData = {gotoRow: this.MIDDLE, gotoCell: this.MIDDLE};
     let button = <button type="button" onClick={() => this.handleNav(navData)} key={buttonKey}>home</button>;
     let subPageTopNav = <div className="sub-page-top-nav" key={subPageTopNavKey}>{button}</div>;
-    let cell = <div className="cell-1x1 color-2" key={cellKey}>{subPageTopNav}</div>;
+    let cell = <div
+      className="cell-1x1 color-2"
+      key={cellKey}
+      >{subPageTopNav}</div>;
     return cell;
   }
 
@@ -61,13 +116,106 @@ class App extends Component {
     // note that gotoCell and gotoRow will be 0, 1 or 2
     let leftPosition = navData.gotoCell * this.getCellWidth();
     let topPosition = navData.gotoRow * this.getCellHeight();
-
     window.scroll({
       top: topPosition,
       left: leftPosition,
       behavior: 'smooth'
     });
     this.log('handleNav, destination = ' + leftPosition + ', ' + topPosition);
+  }
+
+  handleTouchStart = (evt) => {
+    // just want to capture scroll position and the touch position
+      let touch0 = evt.touches[0];
+      this.log('touchStartPosition=(' + touch0.clientX + ',' + touch0.clientY + ') windowPosition=(' + window.pageXOffset + ',' + window.pageYOffset + ')');
+      this.touchStartPosition = [touch0.clientX, touch0.clientY];
+      this.windowPosition = [window.pageXOffset, window.pageYOffset];
+  }
+
+  /*
+  * handleTouchMove - adjusts the scrolling of the window as expected, up until
+  * it reaches a THRESHOLD, then the window srolls all the way to the next grid section
+  */
+  handleTouchMove = (evt) => {
+      this.log('touch move');
+      let newTouchPosition = [evt.touches[0].clientX, evt.touches[0].clientY]
+      //this.log(' -> position = ' + newTouchPosition[0] + ',' + newTouchPosition[1]);
+
+      if (newTouchPosition[0] > ((1 + this.HORIZ_THRESHOLD) * this.getCellWidth())) {
+        this.log(' -> position exceeded HORIZ_THRESHOLD...now we want to scroll');
+        evt.preventDefault();
+        let travelDirection = this.getTravelDirection(newTouchPosition);
+        this.log('handleTouchMove - travelDirection=' + travelDirection);
+        let newWindowPosition = this.getNewWindowPositionFromTravelDirection(travelDirection);
+        window.scroll({
+          top: newWindowPosition[1],
+          left: newWindowPosition[0],
+          behavior: 'smooth'
+        });
+      } else {
+        let moveVector = this.subtractVectors(newTouchPosition, this.touchStartPosition);
+        let reverseMoveVector = this.scaleVector(moveVector, -1);
+        let newScrollPosition = this.addVectors(this.windowPosition, reverseMoveVector);
+        window.scroll({
+          top: newScrollPosition[1],
+          left: newScrollPosition[0]
+        });
+      }
+  }
+
+  handleTouchCancel = (evt) => {
+      this.log('touch cancel');
+  }
+
+  handleTouchEnd = (evt) => {
+      this.log('touch end');
+  }
+
+  /*
+  * getTravelDirection - when the user is in the middle of a touchMove, determine
+  * the compass direction the user is heading in (east, northeast, etc), based on their direction of travel
+  */
+  getTravelDirection = (newTouchPosition) => {
+    if (this.touchStartPosition.length !== 2 || newTouchPosition.length !== 2) {
+      throw new Error('both touchStartPosition and newTouchPosition must be arrays of length 2');
+    }
+    let xDistanceTraveled = newTouchPosition[0] - this.touchStartPosition[0];
+    let yDistanceTraveled = newTouchPosition[1] - this.touchStartPosition[1];
+    let angleOfTravel = Math.atan(yDistanceTraveled / xDistanceTraveled);
+    return this.getCompassPoint(angleOfTravel);
+  }
+
+  /*
+  * given an angle, return the closest compass direction (east, northeast, etc)
+  */
+  getCompassPoint = (angleOfTravel) => {
+    let directionRanges = Object.values(this.DIRECTION_RANGES);
+    let directionNames = Object.keys(this.DIRECTION_RANGES);
+    let directionIndex;
+    for (let i in directionRanges) {
+      // special case for directionRanges[0] which is pointing northeast
+      if (i == 0) {
+        if ((angleOfTravel > directionRanges[i][0] && angleOfTravel <= Math.PI * 2) ||
+          (angleOfTravel >= 0 && angleOfTravel <= directionRanges[i][1])) {
+            directionIndex = i;
+            break;
+          }
+      } else if (angleOfTravel > directionRanges[i][0] && angleOfTravel <= directionRanges[i][1]) {
+        directionIndex = i;
+        break;
+      }
+    }
+    return directionNames[directionIndex];
+  }
+
+  getNewWindowPositionFromTravelDirection = (travelDirection) => {
+    // direction vector is a unit vector. e.g. [1,-1] tells us to move 1 screen width right and 1 screen height down
+    let directionVector = this.DIRECTION_VECTORS[travelDirection];
+    let newWindowPositionX = this.windowPosition[0] + this.getCellWidth() * directionVector[0];
+    let newWindowPositiony = this.windowPosition[1] + this.getCellHeight() * directionVector[1];
+
+    //QQQQ need to correct for going past edges of grid
+    return([newWindowPositionX, newWindowPositiony]);
   }
 
   getCellWidth = () => {
@@ -86,6 +234,27 @@ class App extends Component {
       // document.getElementsByClassName('cell-1x1')[0].scrollHeight;
     }
     return this.cellHeight;
+  }
+
+  addVectors = (vect1, vect2) => {
+    if (vect1.length !== 2 || vect2.length !== 2 ) {
+      throw new Error('both vectors must be arrays of length 2');
+    }
+    return [vect1[0] + vect2[0], vect1[1] + vect2[1]];
+  }
+
+  subtractVectors = (vect1, vect2) => {
+    if (vect1.length !== 2 || vect2.length !== 2 ) {
+      throw new Error('both vectors must be arrays of length 2');
+    }
+    return [vect1[0] - vect2[0], vect1[1] - vect2[1]];
+  }
+
+  scaleVector = (vect1, scalar) => {
+    if (vect1.length !== 2 || typeof scalar != 'number') {
+      throw new Error('the vector must be an array of length 2 and the scalar must be a number');
+    }
+    return [vect1[0] * scalar, vect1[1] * scalar];
   }
 
   log = (message) => {
