46ae0001c274625cff588c2d37759e1bf45a5560
Simon Amarasingham <simon@dsonic.com>
2019-03-30 21:08:07 -0700

    fixing height of side nav divs

--- public/scripts/pages.js
+++ public/scripts/pages.js
@@ -1,53 +1,61 @@
 const pageBuilder = () => {
+    return {
+      boilerplate: function(cellId) {
+        let pageContainer = document.getElementById(cellId);
 
-  return {
-    boilerplate: (cellId) => {
-      let pageContainer = document.getElementById(cellId);
+        let topNav = document.createElement('div');
+        topNav.classList.add('nav');
+        topNav.innerHTML = directions.northwest.html + directions.north.html + directions.northeast.html;
+        pageContainer.appendChild(topNav);
 
-      let topNav = document.createElement('div');
-      topNav.classList.add('nav');
-      topNav.innerHTML = directions.northwest.html + directions.north.html + directions.northeast.html;
-      pageContainer.appendChild(topNav);
+        let pageMiddle = document.createElement('div');
+        pageMiddle.classList.add('page-middle');
+        pageContainer.appendChild(pageMiddle);
 
-      let pageMiddle = document.createElement('div');
-      pageMiddle.classList.add('page-middle');
-      pageContainer.appendChild(pageMiddle);
+        let leftNav = document.createElement('div');
+        leftNav.classList.add('left-nav','align-left');
+        leftNav.innerHTML = directions.west.html;
+        pageMiddle.appendChild(leftNav);
 
-      let leftNav = document.createElement('div');
-      leftNav.classList.add('left-nav','align-left');
-      leftNav.innerHTML = directions.west.html;
-      pageMiddle.appendChild(leftNav);
+        let pageContent = document.createElement('div');
+        pageContent.classList.add('page-content','align-left');
+        pageMiddle.appendChild(pageContent);
 
-      let pageContent = document.createElement('div');
-      pageContent.classList.add('page-content','align-left');
-      pageMiddle.appendChild(pageContent);
+        let rightNav = document.createElement('div');
+        rightNav.classList.add('right-nav');
+        rightNav.innerHTML = directions.east.html;
+        pageMiddle.appendChild(rightNav);
 
-      let rightNav = document.createElement('div');
-      rightNav.classList.add('right-nav');
-      rightNav.innerHTML = directions.east.html;
-      pageMiddle.appendChild(rightNav);
+        let bottomNav = document.createElement('div');
+        bottomNav.classList.add('nav');
+        bottomNav.innerHTML =  directions.southwest.html + directions.south.html + directions.southeast.html;
+        pageContainer.appendChild(bottomNav);
 
-      let bottomNav = document.createElement('div');
-      bottomNav.classList.add('nav');
-      bottomNav.innerHTML =  directions.southwest.html + directions.south.html + directions.southeast.html;
-      pageContainer.appendChild(bottomNav);
+        let sideNavHeight = this.getSideNavHeight(topNav, bottomNav);
+        leftNav.style.heeight = sideNavHeight + 'px';
+        rightNav.style.height = sideNavHeight + 'px';
 
-      return pageContent;
-    },
+        return pageContent;
+      },
 
-    homePage: (pageContentDiv) => {
-      let whatWeDo = document.createElement('div');
-      whatWeDo.classList.add('text-block-med');
-      whatWeDo.innerHTML =
-        "<p>Curated Entertainment is a one-of-a-kind, boutique entertainment company in the San Francisco Bay Area, specializing in custom creating and performing never seen before, immersive and story-driven entertainment programs.<br>" +
-        "<br>Co-founded in 2018 by Mind-Magician & Speaker, Heather Rogers and Vocalist & Composer, Velia Amarasingham, we exist to collaborate with event planners and designers to bring their events' themes to vivid life through deeply customized, entertainment programs featuring music, magic, interactive games, emcee programs, and much, much more.<br>" +
-        "<br>We're the company to call when you're looking for something different.</p>";
-      pageContentDiv.appendChild(whatWeDo);
+      getSideNavHeight: function(topNav, bottomNav) {
+        // window inner height minus the top and bottom nav heights
+        window.innerHeight - topNav.clientHeight - bottomNav.clientHeight;
+      },
 
-      let logo = document.createElement('div');
-      logo.classList.add('curated-logo');
-      logo.innerHTML = '<img src="/img/curated-logo-min.png"/>';
-      pageContentDiv.appendChild(logo);
-    },
-  }
+      homePage: function(pageContentDiv) {
+        let whatWeDo = document.createElement('div');
+        whatWeDo.classList.add('text-block-med');
+        whatWeDo.innerHTML =
+          "<p>Curated Entertainment is a one-of-a-kind, boutique entertainment company in the San Francisco Bay Area, specializing in custom creating and performing never seen before, immersive and story-driven entertainment programs.<br>" +
+          "<br>Co-founded in 2018 by Mind-Magician & Speaker, Heather Rogers and Vocalist & Composer, Velia Amarasingham, we exist to collaborate with event planners and designers to bring their events' themes to vivid life through deeply customized, entertainment programs featuring music, magic, interactive games, emcee programs, and much, much more.<br>" +
+          "<br>We're the company to call when you're looking for something different.</p>";
+        pageContentDiv.appendChild(whatWeDo);
+
+        let logo = document.createElement('div');
+        logo.classList.add('curated-logo');
+        logo.innerHTML = '<img src="/img/curated-logo-min.png"/>';
+        pageContentDiv.appendChild(logo);
+      },
+    }
 }
--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  /***************
-  QQQQQQQ need to set the state somehow - react updates every time the state is change, so make use of that QQQQQQQQQ
-  then need to use state change to update size of cells
-  these need to be changed when the orientation changes
-  also need to take the url window into account and make the cell smaller than 100vh
-  after all that, then need to work out how to display content within the cells, maybe using layers
-  **************/
-  state = {
+  /*state = {
     loggingOn: true,
     nextKey: 0,
     cellWidth: null,
     cellHeight: null,
     windowPosition: [],
     touchStartPosition: [],
-  }
+  }*/
 
   /* don't seem to need this
   constructor(props) {
@@ -30,43 +23,6 @@ class App extends Component {
     console.log('****rendering****');
     return null;
   }
-
-  /* *****
-  * keeping these here for reference in case we need to go back to doing these in react
-
-  makeGrid = () => {
-    let grid = [];
-    for (let i = 0; i < GRID_SIZE; i++) {
-      grid.push(makeOneRow(i));
-    }
-    return grid;
-  }
-
-  makeOneRow = (rowPosition) => {
-    let row = [], children = [];
-    let rowKey = getNextKey();
-    for (let i = 0; i < GRID_SIZE; i++) {
-      children.push(makeOneCell(i, rowPosition));
-    }
-    row.push(<div className="cell-3x1" key={rowKey}>{children}</div>)
-    return row;
-  }
-
-  makeOneCell = (cellPosition, rowPosition) => {
-    let buttonKey = getNextKey();
-    let subPageTopNavKey = getNextKey();
-    let cellKey = getNextKey();
-    let navData = {gotoRow: MIDDLE, gotoCell: MIDDLE};
-    let button = <button type="button" onClick={() => handleNav(navData)} key={buttonKey}>home</button>;
-    let subPageTopNav = <div className="sub-page-top-nav" key={subPageTopNavKey}>{button}</div>;
-    let cell = <div
-      className={'cell-1x1 color-' + cellPosition + '-' + rowPosition}
-      key={cellKey}
-      >{subPageTopNav}</div>;
-    return cell;
-  }
-  ******/
-
 }
 
 export default App;
